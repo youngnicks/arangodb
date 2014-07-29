5a29771ce3a58bd882441cd3ce5ce719510d3fa7
Michael Hackstein <michael@arangodb.com>
2014-07-29 16:06:39 +0200

    Added an implementation of get responsible shards and edge shards, using the original collections on single server and falling back to ClusterInfo on cluster

--- js/server/modules/org/arangodb/pregel.js
+++ js/server/modules/org/arangodb/pregel.js
@@ -29,6 +29,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 var db = require("internal").db;
+var _ = require("underscore");
 
 exports.getServerName = function () {
   return ArangoServerState.id() || "localhost";
@@ -67,7 +68,53 @@ exports.getGlobalCollection = function (executionNumber) {
 };
 
 exports.getResponsibleShard = function (doc) {
-  return ArangoClusterInfo.getResponsibleShard(doc);
+  var info = doc.split("/");
+  var col = info[0];
+  if (ArangoServerState.role() === "PRIMARY") {
+    var key = info[1];
+    return ArangoClusterInfo.getResponsibleShard(col, {_key: key});
+  }
+  return col;
+};
+
+exports.getResponsibleEdgeShards = function (executionNumber, doc) {
+  var globalCol = exports.getGlobalCollection(executionNumber);
+  var col = doc.split("/");
+  var map = globalCol.document("map").map;
+  var example = {
+    _from: doc
+  };
+  var result = [];
+  if(map[col] !== undefined) {
+    if (ArangoServerState.role() === "PRIMARY") {
+      _.each(map, function (c, key) {
+        if (c.type === 3) {
+          result.push(ArangoClusterInfo.getResponsibleShard(key, example));
+        }
+      });
+      return result;
+    }
+    _.each(map, function (c, key) {
+      if (c.type === 3) {
+        result.push(key);
+      }
+    });
+    return result;
+  }
+  if (ArangoServerState.role() === "PRIMARY") {
+    _.each(map, function (c) {
+      if (c.type === 3) {
+        result.push(ArangoClusterInfo.getResponsibleShard(c.resultCollection, example));
+      }
+    });
+    return result;
+  }
+  _.each(map, function (c) {
+    if (c.type === 3) {
+      result.push(c.resultCollection);
+    }
+  });
+  return result;
 };
 
 exports.Conductor = require("org/arangodb/pregel/conductor");
