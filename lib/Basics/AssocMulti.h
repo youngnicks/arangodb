8de50e4b5b4246fb4f7682a5eed4c80089c6dd5d
Max Neunhoeffer <max@arangodb.com>
2015-01-17 00:19:27 +0100

    Switch off debugging.

--- lib/Basics/AssocMulti.h
+++ lib/Basics/AssocMulti.h
@@ -32,10 +32,8 @@
 #ifndef ARANGODB_BASICS_ASSOC_MULTI_H
 #define ARANGODB_BASICS_ASSOC_MULTI_H 1
 
-//#if 0
 // Activate for additional debugging: 
-#define TRI_CHECK_MULTI_POINTER_HASH 1 
-//#endif 
+// #define TRI_CHECK_MULTI_POINTER_HASH 1 
 
 #include "Basics/Common.h"
 #include "Basics/prime-numbers.h"
