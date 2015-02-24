335886619f41f27fd67526f16e5f89e7a84d77e4
Heiko Kernbach <heiko@arangodb.com>
2015-02-24 12:47:51 +0100

    grunt now uses grunt-sass, 1000x faster

--- js/apps/system/_admin/aardvark/APP/GruntFile.js
+++ js/apps/system/_admin/aardvark/APP/GruntFile.js
@@ -311,6 +311,8 @@
       }
     });
 
+    grunt.loadNpmTasks("grunt-sass");
+
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
     grunt.loadNpmTasks('grunt-text-replace');
 
