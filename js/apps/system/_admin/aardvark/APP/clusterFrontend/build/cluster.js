1337e67eeae715708a94775a9b4a82760d83d722
Heiko Kernbach <heiko@arangodb.com>
2015-02-24 17:11:37 +0100

    management view + css

--- js/apps/system/_admin/aardvark/APP/clusterFrontend/build/cluster.js
+++ js/apps/system/_admin/aardvark/APP/clusterFrontend/build/cluster.js
@@ -3823,6 +3823,24 @@ window.StatisticsDescriptionCollection = Backbone.Collection.extend({
 
     render: function () {
       $(this.el).html(this.template.render());
+      this.testFunction();
+      console.log("after render");
+    },
+
+    renderTableLine: function () {
+      var htmlString = '<div class="t-row"><div class="pure-u-3-24"><i class="fa fa-check-circle-o state-green"></i></div>';
+      htmlString += '<div class="pure-u-7-24">Node A</div>';
+      htmlString += '<div class="pure-u-7-24">77%</div>';
+      htmlString += '<div class="pure-u-7-24">65%</div></div>';
+      $('.management-tbody').append(htmlString);
+    },
+
+    testFunction: function() {
+      var i;
+
+      for (i=0; i<10; i++) {
+        this.renderTableLine();
+      }
     }
 
   });
--- js/apps/system/_admin/aardvark/APP/clusterFrontend/js/views/managementView.js
+++ js/apps/system/_admin/aardvark/APP/clusterFrontend/js/views/managementView.js
@@ -16,6 +16,24 @@
 
     render: function () {
       $(this.el).html(this.template.render());
+      this.testFunction();
+      console.log("after render");
+    },
+
+    renderTableLine: function () {
+      var htmlString = '<div class="t-row"><div class="pure-u-3-24"><i class="fa fa-check-circle-o state-green"></i></div>';
+      htmlString += '<div class="pure-u-7-24">Node A</div>';
+      htmlString += '<div class="pure-u-7-24">77%</div>';
+      htmlString += '<div class="pure-u-7-24">65%</div></div>';
+      $('.management-tbody').append(htmlString);
+    },
+
+    testFunction: function() {
+      var i;
+
+      for (i=0; i<10; i++) {
+        this.renderTableLine();
+      }
     }
 
   });
