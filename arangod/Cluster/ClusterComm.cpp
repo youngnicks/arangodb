76f4b8f85013c6b9383a70abb05d08497e610252
Andreas Streichardt <andreas@arangodb.com>
2016-09-26 13:40:22 +0200

    fix conditionlocker

--- arangod/Cluster/ClusterComm.cpp
+++ arangod/Cluster/ClusterComm.cpp
@@ -598,16 +598,14 @@ ClusterCommResult const ClusterComm::wait(
     }
   }
   response = i->second;
-  ClusterCommOpStatus status;
   {
     CONDITION_LOCKER(locker, somethingReceived);
-    status = response.result->status;
-  }
+    ClusterCommOpStatus status = response.result->status;
   
-  while (status == CL_COMM_SUBMITTED) {
-    CONDITION_LOCKER(locker, somethingReceived);
-    somethingReceived.wait(60000000.0);
-    status = response.result->status;
+    while (status == CL_COMM_SUBMITTED) {
+      somethingReceived.wait(100000);
+      status = response.result->status;
+    }
   }
 
   {
